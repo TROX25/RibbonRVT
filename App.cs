0f9e97df6b0c4c549edeae431d42190b1bb081ed
TROX25 <piotrek.dziembowski@gmail.com>
2025-02-12 16:02:16 +0100

    Ribbon + WPF

--- App.cs
+++ App.cs
@@ -8,6 +8,8 @@ using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using System.Windows.Media.Imaging;
 using System.IO;
+using WPF_1;
+using System.Windows;
 #endregion
 
 namespace Ribbon
@@ -17,28 +19,30 @@ namespace Ribbon
         public Result OnStartup(UIControlledApplication a)
         {
             
-                // Create a custom ribbon tab
+                // Ribbon
                 String tabName = "Meet the engineer";
                 a.CreateRibbonTab(tabName);
 
-                // Create two push buttons
+                // Panels
+                RibbonPanel m_projectPanel = a.CreateRibbonPanel(tabName, "Construction tools");
+                RibbonPanel m_projectPanel2 = a.CreateRibbonPanel(tabName, "Export tools");
+
+                // 2 buttons
                 string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
-                PushButtonData button1 = new PushButtonData("Button1", "My Button #1", thisAssemblyPath, "Ribbon.MyTest");
-                PushButtonData button2 = new PushButtonData("Button2", "My Button #2", thisAssemblyPath, "Ribbon.MyTest");
+                PushButtonData button1 = new PushButtonData("Button1", "Rebar count", thisAssemblyPath, "Ribbon.MyTest");
+                PushButtonData button2 = new PushButtonData("Button2", "To Excel", thisAssemblyPath, "Ribbon.Excel");
 
-                // Create a ribbon panel
-                RibbonPanel m_projectPanel = a.CreateRibbonPanel(tabName, "First Panel name");
-                RibbonPanel m_projectPanel2 = a.CreateRibbonPanel(tabName, "Second Panel Name");
-
-
-                // Add the buttons to the panel
+               
+                // Add buttons to the panel
                 PushButton pb1 = m_projectPanel.AddItem(button1) as PushButton;
                 PushButton pb2 = m_projectPanel2.AddItem(button2) as PushButton;
-
+    
                 //Set images for buttons
-                pb1.LargeImage = LoadImage("Ribbon;component/Resources/icon.png");
-                // pb2.LargeImage = LoadImageFromFile(@"E:\ICONS\ace-of-spades-playing-card-25429.png");
-                pb2.LargeImage = LoadImageFromFile(@"E:\ICONS\wrench-and-screwdriver-9453.png");
+                //pb1.LargeImage = LoadImage("Ribbon;component/Resources/icon.png");
+                //pb2.LargeImage = LoadImageFromFile(@"E:\ICONS\ace-of-spades-playing-card-25429.png");
+                //pb1.LargeImage = LoadImageFromFile(@"E:\ICONS\wrench-and-screwdriver-9453.png");
+                pb1.LargeImage = LoadImageFromFile(@"E:\ICONS\iron-bar_5672087.png");
+                pb2.LargeImage = LoadImageFromFile(@"E:\ICONS\Excel-Green.png");
                 return Result.Succeeded;
             
         }
@@ -94,6 +98,16 @@ namespace Ribbon
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
             TaskDialog.Show("123", "TEST TEST TEST \n TEST TEST TEST");
+            return Result.Succeeded;
+        }
+    }
+    [Transaction(TransactionMode.Manual)]
+    public class Excel : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            WPF_1.MainWindow window = new WPF_1.MainWindow();
+            window.ShowDialog();  // Otwarcie okna WPF jako modalne
             return Result.Succeeded;
         }
     }
